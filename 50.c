fc6a3596b36574799dcd7ccb53c08132c902253c
sedhusedhu1996 <30828489+sedhusedhu1996@users.noreply.github.com>
2018-02-02 09:10:01 +0000

    Create 50.c

---
+++ 50.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int c;
+printf("enter the number:");
+scanf("%d",&a);
+if(c%2==0)
+{
+    printf("\nyes");
+}
+else
+{
+    printf("\n no");
+}
+getch();
+}
