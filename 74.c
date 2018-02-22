7c44ca00e0478299ed5d70b8593f623018a34239
sedhusedhu1996 <30828489+sedhusedhu1996@users.noreply.github.com>
2018-02-22 16:13:38 +0530

    Create 74.c

---
+++ 74.c
@@ -0,0 +1,11 @@
+#include <stdio.h
+#include<conio.h>
+#include <math.h>
+ int main()
+{
+       float a=5.4, b=5.6;
+       printf("round of  %f is  %f\n", i, round(a));
+       printf("round of  %f is  %f\n", j, round(b));
+       return 0;
+}
+       
