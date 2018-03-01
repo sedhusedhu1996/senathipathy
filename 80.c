94a87ff3a3d7f3a5da08941335bd254c18b57f0f
sedhusedhu1996 <30828489+sedhusedhu1996@users.noreply.github.com>
2018-03-01 15:50:52 +0530

    Create 80.c

---
+++ 80.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<conio>
+int main()
+{
+		int n,t,rem=0,i;
+    printf("Enter the number");
+    scanf("%d",&n);
+		while(n!=0)
+		{
+			t=n%10;
+			rem=(rem*10)+t;
+			n=n/10;
+		}
+		while(rem!=0)
+		{
+			i=rem%10;
+			rem=rem/10;
+		    if(i%2!=0)
+		    {
+		        printf("%d\n",i);
+		    }
+			
+			}
+      return 0;
+			}
