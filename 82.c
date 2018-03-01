3736edb786b4c1782f4d5862990bc49a738ca353
sedhusedhu1996 <30828489+sedhusedhu1996@users.noreply.github.com>
2018-03-01 16:07:10 +0530

    Create 82.c

---
+++ 82.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main()
+{
+    float a,b,c;
+    printf("enter the area\n");
+   scanf("%f",&a);
+    printf("enter the breath\n");
+    scanf("%f",&b);
+    c=a*b;
+    printf("%f",c);
+}
