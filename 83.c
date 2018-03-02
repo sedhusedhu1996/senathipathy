d31b009e622402844cafff3e583b8d7f546fe74e
sedhusedhu1996 <30828489+sedhusedhu1996@users.noreply.github.com>
2018-03-02 16:09:32 +0530

    Create 83.c

---
+++ 83.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int a,b,i,n;
+char c;
+printf("Enter the Number:");
+scanf("%d",&n);
+for(i=0;i<n;i++)
+{
+scanf("%d%c%d",&a,&c,&b);
+if(i%2==0)
+{
+	printf("\n%d",(a/b));
+}
+else
+{
+printf("\n%d",(a%b));
+}
+}
+getch();
+return 0;
+}
