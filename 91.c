099b57daaa5f5fe40fd94129a1c60530de035527
sedhusedhu1996 <30828489+sedhusedhu1996@users.noreply.github.com>
2018-03-13 16:04:06 +0530

    Create 91.c

---
+++ 91.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+void main()
+{
+    int l,b,h,TSA;
+    printf("enter the surface area of cuboid:\n");
+    scanf("%d%d%d",&l,&b,&h);
+ TSA = l+b+h;
+ printf("Area of surface %d",TSA);
+}
