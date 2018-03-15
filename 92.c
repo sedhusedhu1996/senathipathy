85da40a67ad693e2a674f19ad98ba45e494a7190
sedhusedhu1996 <30828489+sedhusedhu1996@users.noreply.github.com>
2018-03-15 16:13:40 +0530

    Create 92.c

---
+++ 92.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int n,a[10],sum=0,i;
+    printf("Enter the number:\n");
+    scanf("%d",&n);
+    {
+        for(i=0;i<n;i++)
+        scanf("%d",&a[i]);
+            for(i=0;i<n;i++)
+        sum=sum+a[i];
+        printf("total %d",sum);
+    }
+}
