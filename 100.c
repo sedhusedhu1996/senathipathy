17d2ad9eb3ad1eb7d0e70fef03cb26b3b745c4cd
sedhusedhu1996 <30828489+sedhusedhu1996@users.noreply.github.com>
2018-03-15 16:18:26 +0530

    Update and rename 92.c to 100.c

--- 100.c
+++ 100.c
@@ -2,14 +2,14 @@
 #include<conio.h>
 void main()
 {
-    int n,a[10],sum=0,i;
+    int n,a[10],sum=1,i;
     printf("Enter the number:\n");
     scanf("%d",&n);
     {
         for(i=0;i<n;i++)
         scanf("%d",&a[i]);
             for(i=0;i<n;i++)
-        sum=sum+a[i];
+        sum=sum*a[i];
         printf("total %d",sum);
     }
 }
